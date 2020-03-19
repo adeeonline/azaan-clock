6fd1a6cbc95c1484caebf130a2a1d69c91bbbcb1
Assad Ullah <adeeonline@gmail.com>
2020-03-19 02:26:12 -0700

    Added final run event loop

--- azan-cron.py
+++ azan-cron.py
@@ -1,27 +1,52 @@
-from datetime import datetime, time
+from datetime import datetime
 from adhan import adhan
-from adhan.methods import ISNA, ASR_HANAFI
-import pygame
+from adhan.methods import ISNA, ASR_STANDARD
+import sys, random, time, pygame, os, os.path
 
-city_coordinates = (37.3441254,-121.9806518)
-timezone_offset = -8
-params = {}
-params.update(ISNA)
-params.update(ASR_HANAFI)
+def playAzaan(azaan):
+    filePath = './assets/audio/fajr/azan-fajr.mp3'
+    
+    if azaan != "fajr":
+        files = next(os.walk("./assets/audio/"))
+        file_count = len(files[2]) - 1
+        number = random.randint(1, file_count)
+        filePath = './assets/audio/azan-' + number + '.mp3'
 
-adhan_times = adhan(
-    day=datetime.today(),
-    location=city_coordinates,
-    parameters=params,
-    timezone_offset=timezone_offset,
-)
+    pygame.mixer.init()
+    pygame.mixer.music.load(filePath)
+    pygame.mixer.music.play()
+    while pygame.mixer.music.get_busy() == True:
+        continue
 
-del adhan_times["shuruq"]
-print (adhan_times)
-print (datetime.now())
+def getAzaanNameAndTime():
+    coordinates = (37.3441254,-121.9806518)
+    offset = -7
+    params = {}
+    params.update(ISNA)
+    params.update(ASR_STANDARD)
 
-pygame.mixer.init()
-pygame.mixer.music.load('./assets/audio/azan.mp3')
-pygame.mixer.music.play()
-while pygame.mixer.music.get_busy() == True:
-    continue
+    adhanTimes = adhan(
+        day=datetime.today(),
+        location=coordinates,
+        parameters=params,
+        timezone_offset=offset,
+    )
+    del adhanTimes["shuruq"]
+
+    azaanName = ""
+    azaanSleepInterval = 86400
+
+    for key in adhanTimes:
+        d = adhanTimes[key] - datetime.now() 
+        nextAzaanTime = d.total_seconds()
+    
+        if nextAzaanTime < azaanSleepInterval:
+            azaanSleepInterval = nextAzaanTime
+            azaanName = key
+
+    return (azaanName, azaanSleepInterval)
+
+while True:
+    nameAndTime = getAzaanNameAndTime()
+    time.sleep(nameAndTime[1])
+    playAzaan(nameAndTime[0])
