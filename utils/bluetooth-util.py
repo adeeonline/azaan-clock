aef26635a8808a1b5a5ce5831d50558a3e43b832
Assad Ullah <adeeonline@gmail.com>
2020-03-22 00:22:13 -0700

    Added system services

--- utils/bluetooth-util.py
+++ utils/bluetooth-util.py
@@ -1,10 +1,10 @@
 import sys, time, os
 
 while True:
+    time.sleep(300)
     status = os.system('ls /dev/input/ | grep event0 > /dev/null')    
     print(status)
     if status != 0:
         print('Bluetooth is not connected so restarting the service')
         os.system('reboot')
         print('Done service restarting')
-    time.sleep(120)
