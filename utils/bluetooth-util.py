44bf3494e7abe13233ee97ae46f44fc62b760278
Assad Ullah <adeeonline@gmail.com>
2020-03-22 18:56:34 -0700

    Added fixes for rebooting and restarting bluetooth service

--- utils/bluetooth-util.py
+++ utils/bluetooth-util.py
@@ -1,10 +1,17 @@
 import sys, time, os
 
+def bluetoothConnected():
+    status = os.system('ls /dev/input/ | grep event0 > /dev/null')
+    print(status)
+    if status == 0:
+        return True 
+    else:
+        return False
+
 while True:
     time.sleep(300)
-    status = os.system('ls /dev/input/ | grep event0 > /dev/null')    
-    print(status)
-    if status != 0:
+    if bluetoothConnected() == False:
         print('Bluetooth is not connected so restarting the service')
-        os.system('reboot')
-        print('Done service restarting')
+        os.system('sudo systemctl restart bluetooth.service')
+        time.sleep(120)
+        os.system('sudo systemctl reboot')
