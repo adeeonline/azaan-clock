a42b99dd65b2f564f5413d1ffda4a3108998c8d0
Assad Ullah <adeeonline@gmail.com>
2020-03-31 21:26:41 -0700

    Added fixes for Fajr azaan

--- azan-cron.py
+++ azan-cron.py
@@ -1,7 +1,8 @@
-from datetime import datetime
+from datetime import datetime as dt
 from adhan import adhan
 from adhan.methods import ISNA, ASR_HANAFI
-import sys, random, time, pygame, os, os.path
+import datetime, sys, random, time, pygame, os, os.path
+
 
 def playAzaan(azaan):
     dirname = os.path.dirname(__file__)
@@ -21,36 +22,53 @@ def playAzaan(azaan):
     while pygame.mixer.music.get_busy() == True:
         continue
 
-def getAzaanNameAndTime():
-    coordinates = (37.3441254,-121.9806518)
-    offset = -7
-    params = {}
-    params.update(ISNA)
-    params.update(ASR_HANAFI)
 
-    adhanTimes = adhan(
-        day=datetime.today(),
-        location=coordinates,
-        parameters=params,
-        timezone_offset=offset,
-    )
-    del adhanTimes["shuruq"]
-
+def getCurrentAzaan(adhanTimes):
     azaanName = ""
     azaanSleepInterval = 86400
-
+    azaanTime = ""
+    
     for key in adhanTimes:
-        d = adhanTimes[key] - datetime.now() 
+        d = adhanTimes[key] - dt.now() 
         nextAzaanTime = int(d.total_seconds())
     
         if nextAzaanTime < azaanSleepInterval and nextAzaanTime > 0:
             azaanSleepInterval = nextAzaanTime
             azaanName = key
+            azaanTime = adhanTimes[key]
 
-    return (azaanName, azaanSleepInterval)
+    return (azaanName, azaanSleepInterval, azaanTime)
 
-while True:
-    nameAndTime = getAzaanNameAndTime()
+
+def getAzaanNameAndTime(date, coordinates):
+    offset = -7
+    params = {}
+    params.update(ISNA)
+    params.update(ASR_HANAFI)
+    azaanDate = date
+
+    adhanTimes = adhan(
+        day=azaanDate,
+        location=coordinates,
+        parameters=params,
+        timezone_offset=offset,
+    )
+    
+    del adhanTimes["shuruq"]
+    return getCurrentAzaan(adhanTimes)
+    
+
+coordinates = (37.3441254,-121.9806518)
+
+while True:    
+    date = dt.today()
+    nameAndTime = getAzaanNameAndTime(date, coordinates)
+    
+    if nameAndTime[0] == "":
+        date = date + datetime.timedelta(days = 1)
+        nameAndTime = getAzaanNameAndTime(date, coordinates)
+    
     print(nameAndTime)
+
     time.sleep(nameAndTime[1])
     playAzaan(nameAndTime[0])
